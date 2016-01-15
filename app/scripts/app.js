74069c541a1878ff90b2dc804df914d4b912c3c2
Noys Alejandro Sanz <asanz91@gmail.com>
2016-01-15 14:39:55 +0100

    ADD project scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pactometroApp
+ * @description
+ * # pactometroApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pactometroApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pactometroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pactometroApp
+ */
+angular.module('pactometroApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
